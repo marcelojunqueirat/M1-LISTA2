1a5527c3fb67a9686c1baffb26fa92bf2ba477a1
Marcelo Junqueira Teixeira <marcelojxt@gmail.com>
2023-06-07 13:57:07 -0300

    [M1S04] Ex 2

---
+++ [M1S04] Ex 2/script.js
@@ -0,0 +1,11 @@
+alert("Bem vindo ao programa de cálculo de Índice de Massa Corporal (IMC)! Pressione OK para continuar...")
+
+var weight = prompt("Digite o seu peso:")
+var height = prompt("Digite a sua altura:")
+
+weight = weight.replace(",", ".")
+height = height.replace(",", ".")
+
+var resultIMC = Number(weight) / Number(height * height)
+
+alert("O seu Índice de Massa Corporal (IMC) é: " + resultIMC.toFixed(2) + "!")
